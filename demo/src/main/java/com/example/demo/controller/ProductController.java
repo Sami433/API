0da17039c01002a44d219f2bba0dba4d0399fbb7
Sami <votre_email@votre_email.com>
2021-11-02 14:18:03 +0100

    Commit

--- demo/src/main/java/com/example/demo/controller/ProductController.java
+++ demo/src/main/java/com/example/demo/controller/ProductController.java
@@ -20,4 +20,28 @@ public class ProductController {
     public List<Product> list() {
         return service.listAll();
     }
+
+@GetMapping ("/products/{id}")
+    public Product get(@PathVariable Integer id){
+return service.get (id);
+}
+
+
+@PostMapping ("/products")
+    public void add (@RequestBody Product product){
+        service.save (product);
+
+}
+
+@PutMapping ("/products/{id}")
+    public void update (@RequestBody Product product, @PathVariable Integer id){
+        service.save (product);
+
+}
+
+@DeleteMapping ("/products/{id}")
+public void delete (@PathVariable Integer id) {
+        service.delete (id);
+}
+
 }
--- demo/src/main/java/com/example/demo/model/Product.java
+++ demo/src/main/java/com/example/demo/model/Product.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /*@GeneratedValue(strategy = GenerationType.IDENTITY)*/
 
     private Integer id;
     private String name;
